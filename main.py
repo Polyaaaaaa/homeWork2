4d669444128c1ff345a06f41f1294fbdff34a0f1
Polyaa <polinasatrajkina@gmail.com>
2024-09-24 10:04:43 +0300

    add func get_products, new_product

--- main.py
+++ main.py
@@ -12,6 +12,15 @@ class Product:
         self.price = price
         self.quantity = quantity
 
+    @classmethod
+    def new_product(cls, dictionary):
+        name = dictionary.get('name')
+        description = dictionary.get('description')
+        price = dictionary.get('price')
+        quantity = dictionary.get('quantity')
+
+        return cls(name, description, price, quantity)
+
 
 class Category:
     name: str
@@ -23,19 +32,18 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def add_product(self, product):
+        self.__products.append(product)
 
-# if __name__ == "__main__":
-#     product1 = Product("Laptop", "High-end laptop", 1000, 5)
-#     product2 = Product("Smartphone", "Latest model", 500, 10)
-#
-#     category1 = Category("Electronics", "Electronic devices",
-#                          [product1, product2])
-#     category2 = Category("Furniture", "Home furniture", [])
-#
-#     print(f"categories amount: {Category.category_count}")
-#     print(f"products in categories: {Category.product_count}")
+    @property
+    def get_products(self):
+        products = []
+        for product in self.__products:
+            products.append(f'{product.name}, {product.price} руб. Остаток: {product.quantity}')
+
+        return products
